f5bacc581e0db4bf9b3cf76fe8e19c5815aae28d
renierr <github@renier.de>
2025-07-04 21:20:21 +0200

    cleanup unused methods

--- src/runner_tab.py
+++ src/runner_tab.py
@@ -61,18 +61,6 @@ class TabRunnerScript:
         """
         self.driver.execute_script("cleanTabpathVisualization();")
 
-def _get_real_active_element(driver: WebDriver) -> WebElement:
-    # Get the current active element
-    active_element = driver.execute_script("return document.activeElement")
-    # Loop while the current element has an open shadow root with an active element
-    while driver.execute_script(
-        "return arguments[0].shadowRoot ? arguments[0].shadowRoot.activeElement : null;", active_element
-    ):
-        active_element = driver.execute_script(
-            "return arguments[0].shadowRoot.activeElement;", active_element
-        )
-    return active_element
-
 def _collect_elements_by_tab_key(driver: WebDriver) -> list[WebElement]:
     """
     Collects all elements that are focusable by the tab key on the current page.
@@ -132,17 +120,6 @@ def _collect_elements_by_tab_key(driver: WebDriver) -> list[WebElement]:
 
     return focusable_elements
 
-def _filter_stale_elements(driver: WebDriver, elements: list[WebElement]) -> list[WebElement]:
-    valid_elements = []
-    for el in elements:
-        try:
-            # This call ensures the element is still attached by accessing its tag
-            driver.execute_script("return arguments[0].tagName;", el)
-            valid_elements.append(el)
-        except Exception:
-            continue
-    return valid_elements
-
 def runner_tab(config: ProcessingConfig, driver: WebDriver, results: list,
                screenshots_folder: Path, url_idx: int) -> Path|None:
     global tabpath_checker
@@ -155,7 +132,6 @@ def runner_tab(config: ProcessingConfig, driver: WebDriver, results: list,
 
     # send tab keys to page to collect all elements focusable by tab key
     tab_elements = _collect_elements_by_tab_key(driver)
-    #tab_elements = _filter_stale_elements(driver, tab_elements)
     logger.info(f"Found {len(tab_elements)} tabbable elements on page.")
 
     logger.debug(f"Run tab script for url {url_idx} - missing_check={config.missing_tab_check}")
